d680ee3b443c293f6b795c13e3c7a391a4c9f3fe
crystlbrd <e.bitkov@hotmail.de>
2019-07-17 18:31:56 +0200

    defining some API for Table

--- src/Table.php
+++ src/Table.php
@@ -4,8 +4,29 @@ namespace crystlbrd\DatabaseHandler;
 
 class Table
 {
+    private $Connection;
+
+    private $TableName;
+
     public function __construct(IConnection $connection, string $table)
     {
+        // Init
+        $this->Connection = $connection;
+        $this->TableName = $table;
+    }
 
+    public function autoJoin(): bool
+    {
+        // TODO: Implement autojoin()
+    }
+
+    public function join($table, int $joinType = IConnection::JOIN_INNER): bool
+    {
+        // TODO: Implement join()
+    }
+
+    public function select(array $columns = [], array $conditions = [], array $options = []): RowList
+    {
+        // TODO: Implement select
     }
 }
