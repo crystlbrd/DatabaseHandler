8cf54eb9f244dc99ddb3b4c80250fff626aaed88
crystlbrd <e.bitkov@hotmail.de>
2019-08-23 16:33:44 +0200

    Issue #2: Defined an interface

---
+++ src/IRowList.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace crystlbrd\DatabaseHandler;
+
+interface IRowList
+{
+    /**
+     * IRowList constructor.
+     * @param IConnection $connection current connection
+     * @param mixed $data received results
+     */
+    public function __construct(IConnection $connection, $data);
+
+    /**
+     * Fetches a row from the results
+     * @return Row
+     */
+    public function fetch(): Row;
+
+    /**
+     * Returns all rows from the result as a numeric array
+     * @return array
+     */
+    public function fetchAll(): array;
+
+    /**
+     * Returns the amount of rows in the result
+     * @return int
+     */
+    public function countRows(): int;
+
+    /**
+     * Sets the pointer again to the beginning
+     */
+    public function reset(): void;
+
+    /**
+     * Updates all rows
+     * @param array $data array(column => new value)
+     * @return bool
+     */
+    public function update(array $data): bool;
+
+    /**
+     * Deletes all rows from the data source
+     * @return bool
+     */
+    public function delete(): bool;
+}
