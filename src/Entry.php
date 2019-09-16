eece24b8bfbfda74828816255a7aad286ad6f10d
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2019-09-16 13:30:44 +0200

    fixing Iss #17

--- src/Entry.php
+++ src/Entry.php
@@ -24,28 +24,36 @@ class Entry
      */
     protected $Changelist = [];
 
-    public function __construct(Table $table, array $data)
+    public function __construct(Table $table, array $data = [])
     {
         // init
         $this->Table = $table;
         $this->Data = $data;
 
         // parse alias
-        foreach ($this->Data[$this->Table->getTableName()] as $column => $info) {
-            if ($info['alias'] !== $column) {
-                $this->Alias[$info['alias']] = $column;
+        if (!empty($this->Data)) {
+            foreach ($this->Data[$this->Table->getTableName()] as $column => $info) {
+                // move data to 'self'
+                $this->Data['self'][$column] = $info;
+
+                if ($info['alias'] !== $column) {
+                    $this->Alias[$info['alias']] = $column;
+                }
             }
         }
+
+        // unset table reference
+        unset($this->Data[$this->Table->getTableName()]);
     }
 
     public function __set($name, $value)
     {
-        // try to find the column inside the primary table
-        if (isset($this->Data[$this->Table->getTableName()][$name])) {
-            $this->Changelist[$name] = $value;
-        } // try to find the column with its alias
-        else if (isset($this->Alias[$name])) {
+        // try to find the column with its alias
+        if (isset($this->Alias[$name])) {
             $this->Changelist[$this->Alias[$name]] = $value;
+        } // no definition found: just add it to the changelist as it is
+        else {
+            $this->Changelist[$name] = $value;
         }
     }
 
