ae0c82ac56d606f0dccf3a74c7fe9b189720c669
crystlbrd <crystlbrd@bitkov.de>
2020-07-14 19:38:06 +0200

    Working on testing agains a real DB

---
+++ tests/DatabaseTestCase/DatabaseTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace crystlbrd\DatabaseHandler\Tests\DatabaseTestCase;
+
+use PHPUnit\Framework\TestCase;
+
+
+define('DTC_ROOT_DIR', dirname(__FILE__) . '/');
+
+
+class DatabaseTestCase extends TestCase
+{
+}
---
+++ tests/DatabaseTestCase/config/db_credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This file defines the database credentials
+ */
+
+define('DTC_DB_CONN_HOST', 'localhost');
+define('DTC_DB_CONN_USER', 'root');
+define('DTC_DB_CONN_PASS', '');
+define('DTC_DB_CONN_NAME', 'test_databasehandler');
